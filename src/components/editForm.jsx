114add56de431771d943f6675d1d5cc9d41d4428
isaac-glitchcode <isaac.su@gmx.com>
2020-07-20 16:16:24 -0500

    Update User Methods Added

---
+++ src/components/editForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function EditForm(props) {
+    const {name, lastname, email, password} = props.user;
+    
+    return (
+        <div>
+            <h2>Editar un usuario</h2>
+            <form onSubmit={props.updateUser} onInput={props.editInput}>
+            <input name="name" type="text" placeholder="Nombre" value={name}  />
+            <input name="lastname" type="text" placeholder="Apellidos" value={lastname} />
+            <input name="email" type="email" placeholder="Email" value={email}  />
+            <input name="password" type="password" placeholder="Contraseña" value={password} />
+            <input type="submit" value="Actualizar usuario" />
+            </form>
+        </div>
+    )
+}
